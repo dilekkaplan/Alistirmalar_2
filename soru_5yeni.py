0bce54ba974becd1612708cf06794a39daf7ce95
dilekkaplan <72886943+dilekkaplan@users.noreply.github.com>
2020-11-16 22:04:44 +0300

    Update soru_5yeni.py

--- soru_5yeni.py
+++ soru_5yeni.py
@@ -1,58 +1,42 @@
 #Shellde superasal(n) girildiğinde n sayısı super asal ise true, değilse false döndürür.
-#Bu program çalıştırıldığında dört basamaklı süper asal sayıları döndürür.(Beş basamaklı yavaş çalıştı çünkü:O
+#Bu program ise beş basamaklı superasalları dondurur.
 
 
 def superasal(n):
     asal_mi= True
+    
+    if n>9:
+        for i in range(2,n):
 
-    for i in range(1,len(str(n))):
-            if len(str(n))!=1:
-                for bolen in range(2,int(n)):
-                    if n==1:
-                        asal_mi=False
-                        break
+            if  n%i==0:
+                asal_mi=False
+                break
 
-                    if  n%bolen==0:
-                        asal_mi=False
-                        break
+            else:
+                asal_mi=True
 
-                    else:
-                        bolen+=1
-                        asal_mi=True
+        if asal_mi==True:
+            return(superasal(int(str(n)[:-1]))) 
+                       
 
-                if asal_mi==True:
-                    n=int(str(n)[1:])
+    elif n<=9:
+        if n in [2,3,5,7]:
+            asal_mi=True
 
-
-            if len(str(n))==1:
-                if n==1:
-                        asal_mi=False
-                        break
+        else:
+            asal_mi=False                   
                 
-                for bolen in range(2,int(n)):
                     
-                        
-                    if  int(n)%bolen==0:
-                        asal_mi=False
-                        break
-
-                    else:
-                        bolen+=1
-                        asal_mi=True
-
-                if asal_mi==True:
-                    return True
-                else:
-                    return False
-
-                
-for i in range(1000,10000):
-    if superasal(i)==True:
-        print(i,end=" ")
-    
+    if asal_mi==True:
+        return True
+    else:
+        return False
 
 
+for a in range(1000,10000):
+    if superasal(a)==True:
+        print(a,end=" ")
 
-    
+
             
 
